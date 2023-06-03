8926a08d010d1c539e0f6fd0199c9ff464c869e5
aveczzang12 <aveczzang@gmail.com>
2023-06-03 14:35:10 +0900

    메뉴당 주문 가격을 통해서 주문 수량에 따라 주문 금액을 계산하는 코드

--- menu.c
+++ menu.c
@@ -2,37 +2,49 @@
 
 int main() {
     int menu;
+    int quantity;
+    int totalPrice = 0;
 
     printf("커피숍에 오신 것을 환영합니다!\n");
     printf("메뉴를 선택해주세요:\n");
-    printf("1. 아메리카노\n");
-    printf("2. 카페라떼\n");
-    printf("3. 카푸치노\n");
-    printf("4. 모카\n");
-    printf("5. 핫초코\n");
+    printf("1. 아메리카노 - 3000원\n");
+    printf("2. 카페라떼 - 3500원\n");
+    printf("3. 카푸치노 - 4000원\n");
+    printf("4. 모카 - 4500원\n");
+    printf("5. 핫초코 - 3500원\n");
     printf("번호를 입력하세요: ");
     scanf("%d", &menu);
 
+    printf("수량을 입력하세요: ");
+    scanf("%d", &quantity);
+
     switch (menu) {
         case 1:
-            printf("아메리카노를 주문하셨습니다.\n");
+            totalPrice = 3000 * quantity;
+            printf("아메리카노를 %d잔 주문하셨습니다.\n", quantity);
             break;
         case 2:
-            printf("카페라떼를 주문하셨습니다.\n");
+            totalPrice = 3500 * quantity;
+            printf("카페라떼를 %d잔 주문하셨습니다.\n", quantity);
             break;
         case 3:
-            printf("카푸치노를 주문하셨습니다.\n");
+            totalPrice = 4000 * quantity;
+            printf("카푸치노를 %d잔 주문하셨습니다.\n", quantity);
             break;
         case 4:
-            printf("모카를 주문하셨습니다.\n");
+            totalPrice = 4500 * quantity;
+            printf("모카를 %d잔 주문하셨습니다.\n", quantity);
             break;
         case 5:
-            printf("핫초코를 주문하셨습니다.\n");
+            totalPrice = 3500 * quantity;
+            printf("핫초코를 %d잔 주문하셨습니다.\n", quantity);
             break;
         default:
             printf("잘못된 메뉴 번호입니다.\n");
-            break;
+            return 0;
     }
+
+    printf("총 주문 가격은 %d원 입니다.\n", totalPrice);
 
     return 0;
 }
